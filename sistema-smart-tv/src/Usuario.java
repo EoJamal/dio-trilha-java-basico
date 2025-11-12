3599135149bd8a686ccc0a4cb292ff9c2b3e1696
kauan <kauangabrielfrancisco14@gmail.com>
2025-11-12 12:23:04 -0300

    mini-projeto utilizando métodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception{
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(12);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        System.out.println("Tv ligada?" + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual?" + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv ligada?" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> Tv ligada?" + smartTv.ligada);
+    }
+}
